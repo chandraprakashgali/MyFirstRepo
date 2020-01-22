7c7437890716de614bf4d7d2c71c19ea8aee2e55
Your Name <chandraprakash.gali@gmail.com>
2020-01-22 19:44:22 +1100

    commit

---
+++ Pages/LoginPage.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseProgram
+{
+    class LoginPage
+    {
+        public void LoginStep(IWebDriver driver)
+        {
+            driver.FindElement(By.Id("UserName")).SendKeys("hari");
+            driver.FindElement(By.Id("UserName")).SendKeys("123123");
+            driver.FindElement(By.XPath("//input[contains(@type,'submit')]")).Click();
+        }
+    }
+}
